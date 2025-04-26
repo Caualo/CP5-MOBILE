df0df63f481a93c45fac15c78dff1d749547ef5e
Cauã Ferrigolli <145917301+Caualo@users.noreply.github.com>
2025-04-26 20:20:46 -0300

    Add files via upload

---
+++ cp5mobile/src/@types/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: number;
+    title: string;
+    name?: string;
+    poster_path: string;
+    overview: string;
+    vote_average: number;
+    release_date: string;
+  }
+  
